5326be59ed42cf5724724be906c15c7cdd1f4eee
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2023-02-16 06:12:00 +0000

    feat: update scripts

--- scripts/outline/setup.sh
+++ scripts/outline/setup.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-# check sudo user
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-# read variables from .env
 if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
-# configure firewall
-bash ../ufw/setup.sh
-ufw allow $API_PORT/tcp
-ufw allow $MANAGEMENT_PORT/tcp
-ufw allow $MANAGEMENT_PORT/udp
+sudo bash ../ufw/setup.sh
+sudo ufw allow $API_PORT/tcp
+sudo ufw allow $MANAGEMENT_PORT/tcp
+sudo ufw allow $MANAGEMENT_PORT/udp
 
-# remove previous containers if already exist
 docker rm -f shadowbox watchtower
 
-# download install script, run and remove it
 curl https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh --output install_outline.sh
 bash install_outline.sh --keys-port $MANAGEMENT_PORT --api-port $API_PORT --hostname $DOMAIN
 rm install_outline.sh
