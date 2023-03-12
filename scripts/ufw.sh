457253ad2e562f6aee3a1a297d2fe672694160bf
root <keivanipchihagh@gmail.com>
2023-03-12 08:00:49 +0000

    feat(UFW): add K8s ports to firewall

--- scripts/ufw.sh
+++ scripts/ufw.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 ufw enable
+
+# Default policies
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
+
+# Common ports
 ufw allow ssh
 ufw allow http
 ufw allow https
+
+# K8s
+sudo ufw allow 2379/tcp
+sudo ufw allow 6443/tcp
+sudo ufw allow 10250/tcp
