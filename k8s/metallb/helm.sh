9393598bf904779fbd3a6f170648c7d6869637b8
keivan <keivanipchihagh@gmail.com>
2023-04-04 16:23:16 +0000

    init: move files from fleet-infra repo to here

--- k8s/metallb/helm.sh
+++ k8s/metallb/helm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 helm repo add metallb metallb/metallb
 helm repo update
-helm install metallb metallb/metallb --namespace metallb-system
+helm install metallb metallb/metallb --namespace metallb-system --create-namespace
