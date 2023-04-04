a46d6128104a7f1cc83beeec67c1a050434f558f
keivanipchihagh <keivanipchihagh@gmail.com>
2023-04-04 21:50:28 +0000

    feat: add autocomplete for Docker

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,3 +3,7 @@ curl https://get.docker.com/ | bash
 apt install docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
+# Enable autocomplete
+sudo apt-get install bash-completion -y
+curl https://raw.githubusercontent.com/docker/docker-ce/master/components/cli/contrib/completion/bash/docker -o /etc/bash_completion.d/docker.sh
